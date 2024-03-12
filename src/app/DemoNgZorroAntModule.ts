36e53bd81cb764b4df4cf0df52545e8c8719dcc2
Serges YAMAKO <yamakoserges@gmail.com>
2024-03-12 19:16:00 +0100

    add file DemoNgZorroAntModule.ts to Angular Ng Zorro

--- src/app/DemoNgZorroAntModule.ts
+++ src/app/DemoNgZorroAntModule.ts
@@ -1,1 +1,52 @@
+import { NgModule } from "@angular/core";
 
+import{NzAffixModule} from 'ng-zorro-antd/affix';
+import{NzAlertModule} from 'ng-zorro-antd/alert';
+
+
+
+
+
+@NgMdule({
+    exports:[
+        NzAffixModule,
+        NzAlertModule,
+        NzAnchorModule,
+        NzAutocompleteModule,
+        NzAvatarModule,
+        NzBackTopModule,
+        NzBadgeModule,
+        NzButtonModule,
+        NzBreadCrumbModule,
+        NzCalendarModule,
+        nzCardModule,
+        NzCarouselModule,
+        NzCascaderModule,
+        NzCascaderModule,
+        NzCommentModule,
+        NzDatePickerModule,
+        NzDscriptionsModule,
+        NzDividerModule,
+        NzdrawerModule,
+        NzDropDownModule,
+        NzEmptyModule,
+        NzFormModule,
+        NzDridModule,
+        NzI18nModule,
+        NzIconModole,
+        NzImageModule,
+        NzInputModule,
+        NzInputNumeberModule,
+        NzLayoutModule,
+        NzListModule,
+        NzMentionModule,
+        NzMenuModule,
+        NzMessagModule,
+        NzModalModule,
+        NzNoAnimationModule,
+        NzNotificationModule,
+        NzPageHeaderModule,
+        NzPagenationModule,
+        NzPopconfirmModule,
+    ]
+});
