77c6d108597b10130c44839bf7219f61d1bd6125
Serges YAMAKO <yamakoserges@gmail.com>
2024-03-13 18:17:15 +0100

    configuration of the DemoZorroAntModule.ts file and configuration of the company.component.scss file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,1 +1,40 @@
+import { registerLocaleData } from "@angular/common";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./basic/components/login/login.component";
+import { SignupComponent } from "./basic/components/signup/signup.component";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppRoutingModule } from "./app-routing.module";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import { DemoNgZorroAntdModule } from "./DemoNgZorroAntModule";
+import { NgModule } from "@angular/core";
+import { NZ_I18N, en_US } from "ng-zorro-antd/i18n";
 
+
+
+
+
+registerLocaleData(en_US);
+
+@NgModule({
+    declarations:[
+        AppComponent,
+        LoginComponent,
+        SignupComponent,
+    ],
+    imports:[
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        DemoNgZorroAntdModule,
+        ReactiveFormsModule,
+    ],
+    providers:[
+        {provide: NZ_I18N, useValue: en_US}
+    ],
+    bootstrap: [AppComponent]
+})
+export class appModule{}
